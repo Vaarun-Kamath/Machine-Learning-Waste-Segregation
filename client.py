b6b5c1aa4fd7e4fd9f4139b6881a8d0f4d85840b
MaNameEJeff <65110122+MaNameEJeff@users.noreply.github.com>
2023-04-11 18:03:38 +0530

    Working client code which turns motor. Motor angles need refining

---
+++ client.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+import pickle
+import os
+import time
+import struct
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+motor = GPIO.PWM(11, 50)
+motor.start(0)
+time.sleep(2)
+
+
+HEADER=1024
+PORT=10050
+SERVER="192.168.89.239"
+ADDR=(SERVER,PORT)
+
+def turn_servo(x:int, /)->None:
+	global angle
+	angle = x*120
+	print(f'\x1b[41m{angle = }\x1b[0m')
+	motor.ChangeDutyCycle(2+angle/36)
+	time.sleep(0.5)
+	#motor.ChangeDutyCycle(0)
+
+def listen():
+	with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as client:
+		print(client.connect(ADDR))
+		payload_size=struct.calcsize("Q")
+		print("hello")
+		while True:
+			os.system("fswebcam img.jpg")
+			with open("img.jpg","rb") as file:
+				a=pickle.dumps(file.read())
+			message=struct.pack("Q",len(a))+a
+			client.sendall(message)
+			output=int(client.recv(HEADER).decode())
+			turn_servo(output)
+			print(f"\t\x1b[42m{output}\x1b[0m")
+
+
+if __name__ == "__main__":
+	listen()
+
---
+++ testservo.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50)
+
+servo1.start(0)
+print("Waiting for 2 seconds")
+time.sleep(2)
+
+print("Rotating 180 degrees in 10 steps")
+
+duty = 2
+
+while duty <= 12:
+	servo1.ChangeDutyCycle(duty)
+	time.sleep(1)
+	duty = duty + 1
+
+time.sleep(2)
+
+print("Turning back to 90 degrees for 2 seconds")
+servo1.ChangeDutyCycle(7)
+time.sleep(2)
+
+print("Turning back to 0 degrees")
+servo1.ChangeDutyCycle(2)
+time.sleep(0.5)
+servo1.ChangeDutyCycle(0)
+
+servo1.stop()
+GPIO.cleanup()
+print("Goodbye")
